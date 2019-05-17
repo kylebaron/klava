a51a9dd37e879bf029cfa0f954a9d70c450f6b0d
Kyle Baron <kyleb@metrumrg.com>
2019-05-17 08:19:37 -0500

    re-build

--- R/fit.R
+++ R/fit.R
@@ -51,18 +51,33 @@ wls <- function(p, theta, data, pred_name, pred = FALSE,...) {
   ols_impl(data[["DV"]], out[[pred_name]], wt = data[["DV"]])
 }
 
-
+tran_upper <- c("CMT", "II", "ADDL", "EVID", "AMT", "RATE", "MDV", "TIME")
+lctran <- function(data) {
+  n <- names(data)
+  infrom <- is.element(n,c("MDV",tran_upper))
+  haslower <- is.element(tolower(n),n)
+  change <- infrom & !haslower
+  if(sum(change) > 0) names(data)[change] <- tolower(n[change])
+  data
+}
 
 #' @export
-do_fit <- function(theta, data, xtol_rel=1E-6, maxeval=10000,
+nl_optr <- function(theta, data, 
+                   optimizer = c("newuoa", "neldermead", "optim"),
                    pred_name = "CP", ofv = els, logdv = FALSE,
-                   pred_theta = FALSE,
+                   pred_initial = FALSE, cov_step = FALSE,
                    algorithm = "NLOPT_LN_NEWUOA",...) {
+  
+  optimizer <- match.arg(optimizer)
+  
+  opt <- get(optimizer, mode = "function")
+  
   fn <- deparse(substitute(ofv))
-  message("Fitting with ", fn)
+
   ini <- initials(theta)
   message("Checking data ...")
   data <- lctran(data)
+  
   evid_col <- which(names(data)=="evid")[1]
   assert_that(!is.na(evid_col), msg="evid column is required")
   mdv_col <- which(names(data) %in% c("MDV","mdv"))[1]
@@ -74,18 +89,40 @@ do_fit <- function(theta, data, xtol_rel=1E-6, maxeval=10000,
   }
   data <- mutate(data, DV = if_else(mdv==1, NA_real_, DV))
   vdat <- as.data.frame(valid_data_set(data,mod))
-  message("Fitting...")
-  fit <- newuoa(
-    x0=ini, fn=ofv, theta = theta, data = vdat, pred_name=pred_name, logdv = logdv, ...
+  message("Fitting with ", fn, " ...", appendLF=FALSE)
+  fit <- opt(
+    ini, fn=ofv, theta = theta, data = vdat, pred_name=pred_name, logdv = logdv, ...
   ) 
-  message("Generating predicted values ...")
+  message("done.\nGenerating predicttions.")
   pr <- ofv(fit$par, theta=theta, data = vdat, pred_name = pred_name, pred=TRUE)
   data[["PRED"]] <- pr[[pred_name]]
   data[["RES"]] <- data[["DV"]] - data[["PRED"]]
-  if(pred_theta) {
+  if(pred_initial) {
     pr <- ofv(ini, theta = theta,   data = vdat, pred_name = pred_name, pred = TRUE)
     data[["INITIAL"]] <- pr[[pred_name]]
   }
   fit$data <- data
+  coe <- coef(theta)
+  fit$tab <- tibble(par = names(coe), start =ini, final = fit$par)
+  if(cov_step) {
+    message("Trying cov step ... ", appendLF=FALSE)
+    assert_that(requireNamespace("nlme"))
+    co <- try(
+      nlme::fdHess(fit$par, ofv, theta = theta, data = vdat, pred_name = pred_name)
+    )
+    if(inherits(co, "try-error")) {
+      warning("not successful.")  
+    } else {
+      se <- sqrt(diag(solve(co$Hessian)))
+      if(all(!is.nan(se))) {
+        message("success.")  
+      } else {
+        warning("trouble with cov step")  
+      }
+      fit$se <- se
+      fit$tab <- mutate(fit$tab, se = se)
+    }
+  }
+
   fit
 }
